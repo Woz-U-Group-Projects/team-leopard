f72efe6f76c4794b2bbad1b09adcc2f392609e46
Lavota Carter <46871754+kang1lan2@users.noreply.github.com>
2020-02-02 06:33:43 -0500

    missing import

--- server-java-mysql/src/main/java/com/registration/models/RegistrationRepository.java
+++ server-java-mysql/src/main/java/com/registration/models/RegistrationRepository.java
@@ -2,6 +2,8 @@ package com.registration.models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.registration.models.RegistrationModel
+
 public interface RegistrationRepository extends JpaRepository<RegistrationModel, Long> {
 
 }
